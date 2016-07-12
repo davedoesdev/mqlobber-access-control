ffe82672a49512d096c647d70a5eee145f074e9c
David Halls <dahalls@gmail.com>
2016-07-12 22:59:51 +0100

    First cut

---
+++ index.js
@@ -0,0 +1,99 @@
+var QlobberDedup = require('qlobber').QlobberDedup;
+
+function AccessControl(allowed_topics, disallowed_topics)
+{
+    this._allowed_matcher = null;
+    this._disallowed_matcher = null;
+
+    var ths = this;
+
+    function allowed(topic)
+    {
+        if (ths._disallowed_matcher &&
+            ths._disallowed_matcher.match(topic).size > 0)
+        {
+            return false;
+        }
+
+        if (ths._allowed_matcher &&
+            ths._allowed_matcher.match(topic).size === 0)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    this._subscribe_requested = function (topic, done)
+    {
+        if (allowed(topic))
+        {
+            this.subscribe(topic, done);
+        }
+        else
+        {
+            this._warning('blocked subscribe to topic: ' + topic);
+            done();
+        }
+    };
+
+    this._unsubscribe_requested = function (topic, duplex, options, done)
+    {
+        if (allowed(topic))
+        {
+            duplex.pipe(this.fsq.publish(topic, options, done));
+        }
+        else
+        {
+            this._warning('blocked publish to topic: ' + topic);
+            done();
+        }
+    };
+
+    this.reset(allowed_topics, disallowed_topics);
+}
+
+AccessControl.prototype.reset = function (allowed_topics, disallowed_topics)
+{
+    if (allowed_topics &&
+        typeof allowed_topics[Symbol.iterator] === 'function')
+    {
+        this._allowed_matcher = new QlobberDedup();
+        for (var topic of allowed_topics)
+        {
+            this._allowed_matcher.add(topic, true);
+        }
+    }
+    else
+    {
+        this._allowed_matcher = null;
+    }
+
+    if (disallowed_topics &&
+        typeof disallowed_topics[Symbol.iterator] === 'function')
+    {
+        this._disallowed_match = new QlobberDedup();
+        for (var topic of disallowed_topics)
+        {
+            this._disallowed_matcher.add(topic, true);
+        }
+    }
+    else
+    {
+        this._disallowed_matcher = null;
+    }
+};
+
+AccessControl.prototype.attach = function (server)
+{
+    server.on('subscribe_requested', this._subscribe_requested);
+    server.on('publish_requested', this._publish_requested);
+};
+
+AccessControl.prototype.detach = function (server)
+{
+    server.removeListener('subscribe_requested', this._subscribe_requested);
+    server.removeListener('publish_requested', this._publish_requested);
+};
+
+exports.AccessControl = AccessControl;
